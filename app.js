b0303a6cbc37e8f67c940c8ab31b7c8bfc2a0f5b
Jason Kim <jasonkim.0815@gmail.com>
2015-06-14 20:12:21 -0700

    fixed image switching

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
+  var num = 0;
+  var count = 0;
+
   // Load images
   $.get('images.json', function(data) {
-    for (var i = 0; i < data.length; i++) {
-      var image = $('<div class="image"></div>');
+    count = data.length;
+    for (var i = 0; i < count; i++) {
+      var image = $('<div class="image" data-num="' + i + '"></div>');
       image.css('background-image', 'url("' + data[i].src + '")');
       $('body').append(image);
     }
@@ -16,6 +20,7 @@ $(document).ready(function() {
       .css('background-image', target.css('background-image'))
       .removeClass('hide');
     $('body').css('overflow', 'hidden');
+    num = target.attr('data-num');
   });
 
   // Close modal
@@ -31,9 +36,21 @@ $(document).ready(function() {
   });
 
   $('body').keydown(function(e) {
-    if (e.keyCode === 27) {
-      $('#modal').addClass('hide');
-      $('body').css('overflow', 'scroll');
+    switch (e.keyCode) {
+      case 27:
+        $('#modal').addClass('hide');
+        $('body').css('overflow', 'scroll');
+        break;
+      case 37:
+        num = (count+num-1) % count;
+        var image = $('.image[data-num="' + num + '"]');
+        $('#modal').css('background-image', image.css('background-image'));
+        break;
+      case 39:
+        num = (count+parseInt(num)+1) % count;
+        var image = $('.image[data-num="' + num + '"]');
+        $('#modal').css('background-image', image.css('background-image'));
+        break;
     }
   });
 
